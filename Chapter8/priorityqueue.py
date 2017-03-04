d7fd67234f92de0fdbc9991c49f67283648b0afb
Laxmi Thebe <laxmi.thebe@gmail.com>
2017-03-05 00:15:07 +0200

    Note about __slots__ added and implemented for inner Node class

--- Chapter8/priorityqueue.py
+++ Chapter8/priorityqueue.py
@@ -10,12 +10,12 @@ class PriorityQueue:
 
     def __len__(self):
         return len(self._qList)
-
+    #O(n) - but O(1) amortized cost
     def enqueue(self, item, priority):
         """New instance of storage class is created, and appended to the list"""
         entry = self._PriorityQEntry(item, priority)
         self._qList.append(entry)
-
+    #O(n) as it transverses the list in worst possible case
     def dequeue(self):
         """Looks for the entry with highest priority and dequeue"""
         assert not self.isEmpty(), "Empty queue"
@@ -27,8 +27,12 @@ class PriorityQueue:
                 index = i
         entry = self._qList.pop(index)
         return entry.item
-
+    #__slots__ is good when many instances of the class is to be crated
+    #the class should inherit object, and all of the inheritance shoulddelcare
+    #__slot__ and not __dict__
+    #arbirtray attributes can not be added in __slots__ unlike in __dict__ though
     class _PriorityQEntry(object):
+        __slots__ = 'item', 'priority'
         def __init__(self, item, priority):
             self.item = item
             self.priority = priority
